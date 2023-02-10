25fc17b90e739ded26bb7de961c448b7cb796071
Philip Ho <pho86@my.bcit.ca>
2023-02-09 19:21:36 -0800

    initial project setup w ts, sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="description" content="Hello world." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+
+    <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+
+    <Component {...pageProps} />
+
+  </>
+
 }
